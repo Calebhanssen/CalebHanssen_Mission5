7952284622b4650ebb76d0b52ef0a60380e89971
calebhanssen <calebhanssen@gmaill.com>
2024-02-08 11:04:59 -0700

    get crazy

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalebHanssen_Mission5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
